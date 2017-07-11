49d52c35aeb02569c37f09e22804bc5f32352870
RicardoPercy <ricardopee@gmail.com>
2017-07-11 01:14:07 -0400

    sql update?

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+
+INSERT INTO burgers(burger_name, devoured) VALUES('Cheese Burger', FALSE);
+INSERT INTO burgers(burger_name, devoured) VALUES('Ham Burger', FALSE);
+INSERT INTO burgers(burger_name, devoured) VALUES('Pickle Burger', FALSE);
+
+SELECT * FROM burgers_db.burgers;
